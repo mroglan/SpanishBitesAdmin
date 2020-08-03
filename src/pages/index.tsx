5148ddc6c8af0bd2f9ced3b9d9036049d1cfb1d8
Manuel <manueljoseph113@gmail.com>
2020-08-03 08:17:39 -0400

    created template for home admin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,24 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 
     return (
-        <div>
-            hello world
-        </div>
+        <>
+            <Head>
+                <title>Home | Spanish Bites Admin</title>
+            </Head>
+            <div className={styles.root}>
+                <header className={styles.header}>
+                    header
+                </header>
+                <aside className={styles.sideBar}>
+                    side-bar
+                </aside>
+                <main className={styles.main}>
+                    main
+                </main>
+                <footer className={styles.footer}>
+                    footer
+                </footer>
+            </div>
+        </>
     )
 }
