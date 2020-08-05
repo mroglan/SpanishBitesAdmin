e5926fa49cfcc9f0c65b9637ed8c00e9572e5962
Manuel <manueljoseph113@gmail.com>
2020-08-05 19:25:03 -0400

    added BASE_URL env variable

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 module.exports = {
     env: {
-        DATABASE_PASS: process.env.DATABASE_PASS
+        DATABASE_PASS: process.env.DATABASE_PASS,
+        BASE_URL: process.env.BASE_URL
     }
 }
