47ac10fb81959df4b6227b6f0a5d3b912d90eb38
Manuel <manueljoseph113@gmail.com>
2020-08-11 19:25:34 -0400

    added social media to footer

--- src/components/nav/Footer.tsx
+++ src/components/nav/Footer.tsx
@@ -1,9 +1,12 @@
-import {Box, Grid} from '@material-ui/core'
+import {Box, Grid, IconButton} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 const useStyles = makeStyles(theme => ({
     root: {
-        borderTop: `1px solid ${theme.palette.primary.light}`
+        background: theme.palette.primary.dark
     }
 }))
 
@@ -14,13 +17,29 @@ export default function Footer() {
         <Box className={classes.root}>
             <Grid container justify="space-around">
                 <Grid item>
-                    Footer section
-                </Grid>
-                <Grid item>
-                    Footer section
-                </Grid>
-                <Grid item>
-                    Footer section
+                    <Grid container spacing={1}>
+                        <Grid item>
+                            <a href="https://www.youtube.com/channel/UCBRZ200NZVaoyLUfdfAbNpw" target="_blank">
+                                <IconButton style={{color: 'rgba(255, 0, 0)'}} >
+                                    <YouTubeIcon fontSize="large" />
+                                </IconButton>
+                            </a>
+                        </Grid>
+                        <Grid item>
+                            <a href="https://www.facebook.com/spanishsamples/" target="_blank">
+                                <IconButton style={{color: 'rgb(24, 119, 242)'}}>
+                                    <FacebookIcon fontSize="large" />
+                                </IconButton>
+                            </a>
+                        </Grid>
+                        <Grid item>
+                            <a href="https://www.instagram.com/spanishsamples/" target="_blank">
+                                <IconButton style={{color: '#cc2366'}} >
+                                    <InstagramIcon fontSize="large" />
+                                </IconButton>
+                            </a>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </Box>
