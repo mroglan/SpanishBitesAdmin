2691200070f4d8915b0e16a0ae30335dc1a595f6
Manuel <manueljoseph113@gmail.com>
2020-08-11 12:48:50 -0400

    added cloudinary env variables

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ module.exports = {
     env: {
         DATABASE_PASS: process.env.DATABASE_PASS,
         BASE_URL: process.env.BASE_URL,
-        SIGNATURE: process.env.SIGNATURE
+        SIGNATURE: process.env.SIGNATURE,
+        CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET,
+        CLOUDINARY_NAME: process.env.CLOUDINARY_NAME
     }
 }
