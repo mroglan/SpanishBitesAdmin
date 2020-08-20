8b6c89fd16a25fb5ee36a83d9c4195e1f7cd7fae
Manuel <manueljoseph113@gmail.com>
2020-08-20 19:20:38 -0400

    changed author sorting by firstname to be in db query

--- src/utils/authors.ts
+++ src/utils/authors.ts
@@ -4,17 +4,7 @@ import {DBAuthor} from '../database/dbInterfaces'
 export const getAllAuthors = async () => {
     const db = await database()
 
-    const authors:DBAuthor[] = await db.collection('authors').find({}).toArray()
-    // aggregate([
-    //     {
-    //         '$lookup': {
-    //             'from': 'timePeriods',
-    //             'localField': 'timePeriod',
-    //             'foreignField': '_id',
-    //             'as': 'timePeriod'
-    //         }
-    //     }
-    // ]).toArray()
+    const authors:DBAuthor[] = await db.collection('authors').find({}).sort({'firstName': 1}).toArray()
 
     return authors
 }   
