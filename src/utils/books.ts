cf0efea1ee6d257f7eaf06aafde9877cab9fdef8
Manuel <manueljoseph113@gmail.com>
2020-08-20 19:30:22 -0400

    sorting for books by title

--- src/utils/books.ts
+++ src/utils/books.ts
@@ -4,7 +4,7 @@ import {DBBook} from '../database/dbInterfaces'
 export const getAllBooks = async () => {
     const db = await database()
 
-    const books:DBBook[] = await db.collection('books').find({}).toArray()
+    const books:DBBook[] = await db.collection('books').find({}).sort({"title": 1}).toArray()
 
     return books
 }   
