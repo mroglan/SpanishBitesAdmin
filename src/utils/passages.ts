567c26b12f92952674d9a02fc0b8a17b5f04b946
Manuel <manueljoseph113@gmail.com>
2020-08-20 19:31:32 -0400

    sorting for passages by name

--- src/utils/passages.ts
+++ src/utils/passages.ts
@@ -4,7 +4,7 @@ import {DBPassage} from '../database/dbInterfaces'
 export const getAllPassages = async () => {
     const db = await database()
 
-    const passages:DBPassage[] = await db.collection('passages').find({}).toArray()
+    const passages:DBPassage[] = await db.collection('passages').find({}).sort({"name": 1}).toArray()
 
     return passages
 }
