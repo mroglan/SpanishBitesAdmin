22744fd8018c646de0e2dab41a8fea226441c10b
Manuel <manueljoseph113@gmail.com>
2020-08-20 19:28:35 -0400

    sorting for genres by name

--- src/utils/genres.ts
+++ src/utils/genres.ts
@@ -4,7 +4,7 @@ import {DBGenre} from '../database/dbInterfaces'
 export const getAllGenres = async () => {
     const db = await database()
 
-    const genres:DBGenre[] = await db.collection('genres').find({}).toArray()
+    const genres:DBGenre[] = await db.collection('genres').find({}).sort({"name": 1}).toArray()
 
     return genres
 }
