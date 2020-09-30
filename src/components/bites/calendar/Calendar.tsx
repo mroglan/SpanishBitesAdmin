c68dd27cbae654514f0f4a0deb821e1f4a8553d8
Manuel <manueljoseph113@gmail.com>
2020-09-29 21:00:27 -0400

    added autofilling

--- src/components/bites/calendar/Calendar.tsx
+++ src/components/bites/calendar/Calendar.tsx
@@ -8,6 +8,8 @@ import Header from './Header'
 import WeekDays from './WeekDays'
 import Cells from './Cells'
 import EventModal from './EventModal1'
+import {Box} from '@material-ui/core'
+import {SuccessButton} from '../../items/buttons'
 
 interface Props {
     bites: ClientSpanishBite[];
@@ -23,9 +25,11 @@ export default function Calendar({bites, events:dbEvents}:Props) {
             return {date: new Date(event.date), 
             name: bites.find(bite => bite._id === event.bite)?.name,
             bite: event.bite
-        }
-        })
+            }
+        }).sort((a, b) => a.date.getTime() - b.date.getTime())
     }, [rawEvents])
+
+    console.log('events', events)
 
     const closeModal = () => setModalStates({...modalStates, open: false})
 
@@ -63,12 +67,72 @@ export default function Calendar({bites, events:dbEvents}:Props) {
         })
     }
 
+    const [disableAutofill, setDisableAutofill] = useState(false)
+
+    const autofillMonth = async () => {
+        setDisableAutofill(true)
+
+        const monthStart = dateFns.startOfMonth(currentDate)
+        const monthEnd = dateFns.endOfMonth(currentDate)
+
+        const eventsFilled = events.filter(event => dateFns.isSameMonth(currentDate, event.date) && event.bite)
+
+        let unusedBiteIds = bites.filter(bite => !eventsFilled.find(event => event.bite === bite._id)).map(bite => bite._id)
+        const datesFilled = eventsFilled.map(event => event.date)
+
+        let day = monthStart
+        const newEvents = []
+
+        while(day <= monthEnd) {
+
+            if(datesFilled.find(date => dateFns.isSameDay(date, day))) { // date already has a bite
+                day = dateFns.addDays(day, 1)
+                continue
+            }
+
+            if(unusedBiteIds.length === 0) { // not enough bites for no repeats
+                unusedBiteIds = bites.map(bite => bite._id)
+            }
+
+            const randomIndex = Math.round(Math.random() * (unusedBiteIds.length - 1))
+            const newBiteId = unusedBiteIds[randomIndex]
+
+            newEvents.push({
+                date: day,
+                bite: newBiteId
+            })
+
+            unusedBiteIds.splice(randomIndex, 1)
+            day = dateFns.addDays(day, 1)
+        }
+
+        await axios({
+            method: 'POST',
+            url: '/api/dailyevent',
+            data: {
+                operation: 'insertMany',
+                events: newEvents
+            }
+        })
+
+        trigger('/api/dailyevent')
+
+        setDisableAutofill(false)
+    }
+
     return (
-        <div className={styles.calendar}>
-            <Header currentDate={currentDate} setCurrentDate={setCurrentDate} />
-            <WeekDays currentDate={currentDate} />
-            <Cells events={events} currentDate={currentDate} selectedDate={selectedDate} onDateClick={onDateClick} />
-            <EventModal events={events} bites={bites} modalStates={modalStates} />
+        <div>
+            <div className={styles.calendar}>
+                <Header currentDate={currentDate} setCurrentDate={setCurrentDate} />
+                <WeekDays currentDate={currentDate} />
+                <Cells events={events} currentDate={currentDate} selectedDate={selectedDate} onDateClick={onDateClick} />
+                <EventModal events={events} bites={bites} modalStates={modalStates} />
+            </div>
+            <Box my={1} textAlign="center">
+                <SuccessButton disabled={disableAutofill} variant="outlined" onClick={() => autofillMonth()} >
+                    Autofill remainder of month
+                </SuccessButton>
+            </Box>
         </div>
     )
 }
--- src/components/bites/calendar/EventModal1.tsx
+++ src/components/bites/calendar/EventModal1.tsx
@@ -1,6 +1,7 @@
 import {ClientSpanishBite, ClientDailyEvent} from '../../../database/dbInterfaces'
 import {useState, useMemo} from 'react'
-import {Dialog, DialogTitle, DialogActions, DialogContent, Box, FormControl, InputLabel, Select, MenuItem} from '@material-ui/core'
+import {Dialog, DialogTitle, DialogActions, DialogContent, Box, FormControl, InputLabel, Select, 
+    MenuItem, Grid, Radio, Typography, InputBase} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
 import {SuccessButton, ErrorButton} from '../../items/buttons'
 import {format, isSameDay} from 'date-fns'
@@ -8,6 +9,14 @@ import {format, isSameDay} from 'date-fns'
 const useStyles = makeStyles(theme => ({
     select: {
         minWidth: 250
+    },
+    rangeInput: {
+        maxWidth: '3ch',
+        background: 'rgba(0, 0, 0, 0.3)',
+        borderRadius: 5,
+        '& input': {
+            textAlign: 'center'
+        }
     }
 }))
 
@@ -29,9 +38,29 @@ export default function EventModal({bites, events, modalStates: {date, open, onS
     
     const [id, setId] = useState(events.find(event => isSameDay(event.date, date))?.bite || '')
 
+    const [auto, setAuto] = useState(false)
+    const [dayRange, setDayRange] = useState(14)
+
     useMemo(() => {
         setId(events.find(event => isSameDay(event.date, date))?.bite || '')
+        setAuto(false)
     }, [open])
+
+    useMemo(() => {
+        if(!auto) return
+        const milliSecondRange = dayRange * 8.64e7
+        const reverseEvents = events.reverse()
+        let biteIds = bites.map(bite => bite._id)
+        for(const event of reverseEvents) {
+            const diff = date.getTime() - event.date.getTime()
+            if(diff > milliSecondRange) break // all remaining values will have even greater diff
+            if(diff < 0) continue // this event's bite comes later, not previous to current date
+            biteIds = biteIds.filter(id => id !== event.bite)
+        }
+        if(biteIds.length === 0) return
+        const randomIndex = Math.round(Math.random() * (biteIds.length - 1))
+        setId(biteIds[randomIndex])
+    }, [auto])
 
     const classes = useStyles()
     return (
@@ -51,6 +80,30 @@ export default function EventModal({bites, events, modalStates: {date, open, onS
                         </FormControl>
                     </Box>
                 </form>
+                <Box my={2}>
+                    <Grid container alignItems="center" wrap="nowrap">
+                        <Grid item>
+                            <Radio value={true} checked={auto} onChange={() => setAuto(!auto)} />
+                        </Grid>
+                        <Grid item style={{flexGrow: 1}}>
+                            <Grid container spacing={1} alignItems="center" wrap="nowrap">
+                                <Grid item>
+                                    <Typography variant="body1">
+                                        Auto populate with bite not used in last
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+                                    <InputBase className={classes.rangeInput} value={dayRange} onChange={(e) => setDayRange(Number(e.target.value))} />
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant="body1">
+                                        days.
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Box>
             </DialogContent>
             <DialogActions style={{justifyContent: 'flex-start'}} >
                 <Box pl={2}>
