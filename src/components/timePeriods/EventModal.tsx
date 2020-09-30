f2b6ad321ed7f1ced47cdd3fa8a14f89e253e185
Manuel <manueljoseph113@gmail.com>
2020-09-30 11:31:15 -0400

    added image uplaoding

--- src/components/timePeriods/EventModal.tsx
+++ src/components/timePeriods/EventModal.tsx
@@ -1,9 +1,10 @@
-import {Dialog, DialogTitle, DialogActions, DialogContent, Box, TextField, Typography, Divider} from '@material-ui/core'
+import {Dialog, DialogTitle, DialogActions, DialogContent, Box, TextField, Typography, Divider, Grid, Button} from '@material-ui/core'
 import {SuccessButton} from '../items/buttons'
 import {BasicTextEditor} from '../items/TextEditor'
 import {makeStyles} from '@material-ui/core/styles'
 import {Event} from '../../database/dbInterfaces'
-import {useState, useMemo} from 'react'
+import {useState, useMemo, useRef} from 'react'
+import {uploadImage} from '../../utils/images'
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -33,8 +34,13 @@ export default function EventModal({initialValues, eventStates: {type, open, onS
         setValues(initialValues)
     }, [open])
 
-    console.log('desc', values.desc)
-    console.log(index)
+    const imageRef = useRef<HTMLInputElement>()
+
+    const handleFileUpoad = async (e) => {
+        const file = e.target.files[0]
+        const url = await uploadImage(file)
+        setValues({...values, image: url})
+    }
 
     const classes = useStyles()
     return (
@@ -43,10 +49,6 @@ export default function EventModal({initialValues, eventStates: {type, open, onS
             <DialogTitle style={{textAlign: 'center'}}>{type === 'create' ? 'Create an Event' : 'Edit Event'}</DialogTitle>
             <DialogContent dividers>
                 <form>
-                    {/*<Box my={2}>
-                        <TextField variant="outlined" color="secondary" label="Event Title" className={classes.textField}
-                        value={values.title} onChange={(e) => setValues({...values, title: e.target.value})} />
-                    </Box>*/}
                     <Box my={2}>
                         <TextField label="Event Year" variant="outlined" color="secondary"
                         value={values.date} onChange={(e) => setValues({...values, date: e.target.value.toString()})} />
@@ -55,9 +57,23 @@ export default function EventModal({initialValues, eventStates: {type, open, onS
                         <TextField variant="outlined" color="secondary" label="Event Location" className={classes.textField}
                         value={values.location} onChange={(e) => setValues({...values, location: e.target.value})} />
                     </Box>
+                    <Divider />
                     <Box my={2}>
-                        <TextField variant="outlined" color="secondary" label="Image URL" className={classes.textField}
-                        value={values.image} onChange={(e) => setValues({...values, image: e.target.value})} />
+                        <input type="file" ref={imageRef} style={{width: 0, height: 0, position: 'fixed'}} onChange={handleFileUpoad} />
+                        <Grid container spacing={2}>
+                            <Grid item>
+                                <Button variant="outlined" onClick={() => imageRef.current.click()}>
+                                    Change Image
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                {values.image && <a style={{textDecoration: 'none'}} href={values.image} target="_blank">
+                                    <Button variant="outlined">
+                                        View Current Image
+                                    </Button>
+                                </a> }
+                            </Grid>
+                        </Grid>
                     </Box>
                     <Divider />
                     <Box my={2} pr={3}>
