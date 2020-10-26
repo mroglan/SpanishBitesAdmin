9baa7fa568de853c4993b89da6b17342444256e0
Manuel <manueljoseph113@gmail.com>
2020-10-26 19:55:49 -0400

    linked to section 1 + 2

--- src/components/blog/edit/PostForm.tsx
+++ src/components/blog/edit/PostForm.tsx
@@ -2,8 +2,11 @@ import {BlogPost} from '../../../database/dbInterfaces'
 import {useState, useCallback} from 'react'
 import Wizard from '../../items/FormWizard'
 import {Box} from '@material-ui/core'
+import {useReducer} from 'react'
+import BasicInfoStep from './BasicInfoStep'
+import ContentStep from './ContentStep'
 
-interface Values extends BlogPost {
+export interface Values extends BlogPost {
     _id?: string;
 }
 
@@ -15,17 +18,38 @@ const sections = [
     'Basic Info', 'Content', 'Review'
 ]
 
-export default function PostForm({values}:Props) {
+const valuesReducer = (state:Values, {type, payload}) => {
+    switch(type) {
+        case 'CHANGE_TITLE':
+            return {...state, title: payload}
+        case 'CHANGE_SUBTITLE':
+            return {...state, subtitle: payload}
+        case 'CHANGE_DATE':
+            return {...state, releaseDate: payload}
+        case 'CHANGE_CONTENT':
+            return {...state, content: payload}
+        default:
+            return state
+    }
+}
+
+export default function PostForm({values:initialVals}:Props) {
+
+    const [values, valuesDispatch] = useReducer(valuesReducer, initialVals)
 
     const [section, setSection] = useState(0)
 
     const changeStep = (diff:number) => setSection(section + diff)
 
     return (
-        <div>
+        <Box>
             <Box>
                 <Wizard sections={sections} sectionNum={section} changeStep={changeStep} />
             </Box>
-        </div>
+            <Box>
+                {section === 0 ? <BasicInfoStep values={values} dispatch={valuesDispatch} /> : 
+                section === 1 ? <ContentStep values={values} dispatch={valuesDispatch} /> : ''}
+            </Box>
+        </Box>
     )
 }
