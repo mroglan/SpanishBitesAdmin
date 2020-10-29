b0522eb5b6f4a749e56401cd143007355a8e2afc
Manuel <manueljoseph113@gmail.com>
2020-10-29 18:34:05 -0400

    seperated update and add functions

--- src/utils/blogPosts.ts
+++ src/utils/blogPosts.ts
@@ -3,9 +3,17 @@ import {Values} from '../components/blog/edit/PostForm'
 
 export const updateBlogPost = async (values:Values) => {
 
-    const {_id} = values
+    const db = await database()
+    
+    await db.collection('blogPosts').updateOne({'_id': values._id}, {'$set': {...values}})
+}
+
+export const createBlogPost = async(values:Values) => {
 
     const db = await database()
 
-    await db.collection('blogPosts').updateOne({'_id': _id}, {'$set': {...values}}, {upsert: true})
+    const cleanedValues = {...values}
+    delete cleanedValues._id
+
+    await db.collection('blogPosts').insertOne({...cleanedValues})
 }
