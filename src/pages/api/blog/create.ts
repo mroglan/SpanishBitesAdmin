189d7481be5ea17b03c658ed576b79dd1be792ab
Manuel <manueljoseph113@gmail.com>
2020-10-29 18:16:22 -0400

    created api to create blog post

---
+++ src/pages/api/blog/create.ts
@@ -0,0 +1,16 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import database from '../../../database/database'
+import {updateBlogPost} from '../../../utils/blogPosts'
+
+export default async function CreateBlog(req:NextApiRequest, res:NextApiResponse) {
+
+    try {
+
+        await updateBlogPost(req.body.values)
+
+        return res.status(200).json({msg: 'Successful creation!'})
+    } catch(e) {
+
+        return res.status(500).json({msg: 'Internal Server Error'})
+    }
+}
