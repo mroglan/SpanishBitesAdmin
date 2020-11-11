288ab05327bb4aec9bbcc71e04547a177093b786
Manuel <manueljoseph113@gmail.com>
2020-11-10 20:17:21 -0500

    added styling for list items

--- src/components/blog/PostList.tsx
+++ src/components/blog/PostList.tsx
@@ -1,19 +1,31 @@
 import {ClientBlogPost} from '../../database/dbInterfaces'
 import {Box, Grid, Typography} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
 import Link from 'next/link'
 
 interface Props {
     posts: ClientBlogPost[];
 }
 
+const useStyles = makeStyles(theme => ({
+    container: {
+        background: '#fff',
+        padding: theme.spacing(2),
+        borderRadius: theme.spacing(.5),
+        marginBottom: theme.spacing(1)
+    }
+}))
+
 export default function PostList({posts}:Props) {
+
+    const classes = useStyles()
 
     return (
         <Box>
             {posts.map((post, i) => (
                 <Link key={i} href="/blog/[id]" as={`/blog/${post._id}`}>
                     <a style={{textDecoration: 'none', color: 'inherit'}}>
-                        <Box key={i}>
+                        <Box className={classes.container} key={i}>
                             <Grid container spacing={3} alignItems="center">
                                 <Grid item style={{flexGrow: 1}}>
                                     <Box>
