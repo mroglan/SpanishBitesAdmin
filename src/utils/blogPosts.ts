6a00b1c167819e62cf8f4c933492e81e60347d91
Manuel <manueljoseph113@gmail.com>
2020-11-10 20:19:04 -0500

    created getBlogPost and fixed updateBlogPost function

--- src/utils/blogPosts.ts
+++ src/utils/blogPosts.ts
@@ -1,12 +1,15 @@
 import database from '../database/database'
 import {Values} from '../components/blog/edit/PostForm'
 import {DBBlogPost} from '../database/dbInterfaces'
+import {ObjectId} from 'mongodb'
 
 export const updateBlogPost = async (values:Values) => {
 
     const db = await database()
+
+    const dbValues = {...values, _id: new ObjectId(values._id)}
     
-    await db.collection('blogPosts').updateOne({'_id': values._id}, {'$set': {...values}})
+    await db.collection('blogPosts').updateOne({'_id': dbValues._id}, {'$set': {...dbValues}})
 }
 
 export const createBlogPost = async(values:Values) => {
@@ -28,4 +31,13 @@ export const getAllPosts = async () => {
     const sortedPosts = posts.sort((a, b) => new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime())
 
     return sortedPosts
+}
+
+export const getBlogPost = async (id:string) => {
+    
+    const db = await database()
+
+    const post:DBBlogPost = await db.collection('blogPosts').findOne({'_id': new ObjectId(id)})
+
+    return post
 }
