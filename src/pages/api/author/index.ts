ab75ca4cf4df58bc7ffed98750202b65bbc1c133
Manuel <manueljoseph113@gmail.com>
2020-11-15 13:05:26 -0500

    imported add, delete, modify functions

--- src/pages/api/author/index.ts
+++ src/pages/api/author/index.ts
@@ -1,42 +1,6 @@
 import {NextApiRequest, NextApiResponse} from 'next'
-import database from '../../../database/database'
-import {Author, DBAuthor} from '../../../database/dbInterfaces'
-import {getAllAuthors} from '../../../utils/authors'
+import {getAllAuthors, addAuthor, modifyAuthor, deleteAuthor} from '../../../utils/authors'
 import {verifyAdmin} from '../../../utils/auth'
-import {ObjectId} from 'mongodb'
-
-interface Values extends Author {
-    timePeriod: string;
-}
-
-const objectifyValues = (values:Values) => {
-    if(!values.timePeriod) return values
-    return {...values, timePeriod: new ObjectId(values.timePeriod)}
-}
-
-const addAuthor = async (values:Values) => {
-    const db = await database()
-
-    const objectVals = objectifyValues(values)
-
-    const dbOperation = await db.collection('authors').insertOne(objectVals)
-
-    return <DBAuthor>dbOperation.ops[0]
-}
-
-const modifyAuthor = async (id:string, values:Values) => {
-    const db = await database()
-
-    const objectVals = objectifyValues(values)
-
-    await db.collection('authors').updateOne({'_id': new ObjectId(id)}, {'$set': {...objectVals}})
-}
-
-const deleteAuthor = async (id:string) => {
-    const db = await database()
-
-    await db.collection('authors').deleteOne({'_id': new ObjectId(id)})
-}
 
 export default verifyAdmin(async function author(req:NextApiRequest, res:NextApiResponse) {
 
