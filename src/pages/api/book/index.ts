98ec64b700f6f69265d4d0efec5123289e09562a
Manuel <manueljoseph113@gmail.com>
2020-11-15 13:29:03 -0500

    imported add, modify, delete functions

--- src/pages/api/book/index.ts
+++ src/pages/api/book/index.ts
@@ -1,45 +1,6 @@
 import {NextApiRequest, NextApiResponse} from 'next'
-import database from '../../../database/database'
-import {ClientBook, DBBook} from '../../../database/dbInterfaces'
-import {getAllBooks} from '../../../utils/books'
+import {getAllBooks, createBook, updateBook, deleteBook} from '../../../utils/books'
 import {verifyAdmin} from '../../../utils/auth'
-import {ObjectId} from 'mongodb'
-
-interface Values extends Omit<ClientBook, '_id'> {}
-
-const objectifyValues = (values:Values) => {
-    if(!values.authors || !values.genres || !values.timePeriod) return values
-    return {
-        ...values,
-        authors: values.authors.map(author => new ObjectId(author)),
-        genres: values.genres.map(genre => new ObjectId(genre)),
-        timePeriod: new ObjectId(values.timePeriod)
-    }
-}
-
-const createBook = async (values:Values) => {
-    const db = await database()
-
-    const objectIdVals = objectifyValues(values)
-
-    const dbOperation = await db.collection('books').insertOne(objectIdVals)
-
-    return <DBBook>dbOperation.ops[0]
-}
-
-const updateBook = async (id:string, values:Values) => {
-    const db = await database()
-
-    const objectIdVals = objectifyValues(values)
-
-    await db.collection('books').updateOne({'_id': new ObjectId(id)}, {'$set': {...objectIdVals}})
-}
-
-const deleteBook = async (id:string) => {
-    const db = await database()
-
-    await db.collection('books').deleteOne({'_id': new ObjectId(id)})
-}
 
 export default verifyAdmin(async function book(req:NextApiRequest, res:NextApiResponse) {
 
