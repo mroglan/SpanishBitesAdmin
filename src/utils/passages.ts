5c088986df8f26d234090d9c91345585594574aa
Manuel <manueljoseph113@gmail.com>
2020-11-15 15:00:22 -0500

    added add, modify, delete functions

--- src/utils/passages.ts
+++ src/utils/passages.ts
@@ -1,5 +1,34 @@
 import database from '../database/database'
-import {DBPassage} from '../database/dbInterfaces'
+import {DBPassage, ClientPassage} from '../database/dbInterfaces'
+import {ObjectId} from 'mongodb'
+
+interface Values extends Omit<ClientPassage, '_id'> {}
+
+const objectifyValues = (values:Values) => {
+    if(!values.book) return values
+
+    return {...values, book: new ObjectId(values.book)}
+}
+
+export const createPassage = async (values:Values) => {
+    const db = await database()
+
+    const dbOperation = await db.collection('passages').insertOne(objectifyValues(values))
+
+    return <DBPassage>dbOperation.ops[0]
+}
+
+export const modifyPassage = async (id:string, values:Values) => {
+    const db = await database()
+
+    await db.collection('passages').updateOne({'_id': new ObjectId(id)}, {'$set': {...objectifyValues(values)}})
+}
+
+export const deletePassage = async (id:string) => {
+    const db = await database()
+
+    await db.collection('passages').deleteOne({'_id': new ObjectId(id)})
+}
 
 export const getAllPassages = async () => {
     const db = await database()
