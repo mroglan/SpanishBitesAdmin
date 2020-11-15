5d75d25989bdd2cf876a9ba4e77d494df52edeb6
Manuel <manueljoseph113@gmail.com>
2020-11-14 19:20:36 -0500

    added try catch to api request

--- src/components/authors/ModifyAuthor.tsx
+++ src/components/authors/ModifyAuthor.tsx
@@ -44,22 +44,24 @@ export default function ModifyAuthor({authors, authorIndex, timePeriods}:Props) 
         const valueCopy = {...values}
         delete valueCopy._id
 
-        const {status} = await axios({
-            method: 'POST',
-            url: '/api/author',
-            data: {
-                operation: 'modify',
-                values: valueCopy,
-                id: values._id
-            }
-        })
-
-        setLoading(false)
-
-        if(status !== 200) {
+        try {
+            await axios({
+                method: 'POST',
+                url: '/api/author',
+                data: {
+                    operation: 'modify',
+                    values: valueCopy,
+                    id: values._id
+                }
+            })
+        } catch(e) {
+            setLoading(false)
             setMessage({type: 'error', content: 'Error Saving'})
             return
         }
+
+        setLoading(false)
+
         setMessage({type: 'success', content: 'Changes Saved'})
         const authorsCopy = [...authors]
         authorsCopy[authorIndex] = values
@@ -77,21 +79,23 @@ export default function ModifyAuthor({authors, authorIndex, timePeriods}:Props) 
             return
         }
 
-        const {status} = await axios({
-            method: 'POST',
-            url: '/api/author',
-            data: {
-                operation: 'delete',
-                id: values._id
-            }
-        })
-
-        setLoading(false)
-
-        if(status !== 200) {
+        try {
+            await axios({
+                method: 'POST',
+                url: '/api/author',
+                data: {
+                    operation: 'delete',
+                    id: values._id
+                }
+            })
+        } catch(e) {
+            setLoading(false)
             setMessage({type: 'error', content: 'Error deleting'})
             return
         }
+
+        setLoading(false)
+        
         setMessage({type: 'success', content: 'Author Deleted'})
 
         valuesDispatch({type: 'CLEAR_VALUES', payload: {}})
