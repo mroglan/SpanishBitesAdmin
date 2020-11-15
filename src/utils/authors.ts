794ff3bf0896bfee4ee67c772ae243fab75f4721
Manuel <manueljoseph113@gmail.com>
2020-11-15 13:05:48 -0500

    added add, modify, delete functions

--- src/utils/authors.ts
+++ src/utils/authors.ts
@@ -1,5 +1,39 @@
 import database from '../database/database'
-import {DBAuthor} from '../database/dbInterfaces'
+import {DBAuthor, Author} from '../database/dbInterfaces'
+import {ObjectId} from 'mongodb'
+
+interface Values extends Author {
+    timePeriod: string;
+}
+
+const objectifyValues = (values:Values) => {
+    if(!values.timePeriod) return values
+    return {...values, timePeriod: new ObjectId(values.timePeriod)}
+}
+
+export const addAuthor = async (values:Values) => {
+    const db = await database()
+
+    const objectVals = objectifyValues(values)
+
+    const dbOperation = await db.collection('authors').insertOne(objectVals)
+
+    return <DBAuthor>dbOperation.ops[0]
+}
+
+export const modifyAuthor = async (id:string, values:Values) => {
+    const db = await database()
+
+    const objectVals = objectifyValues(values)
+
+    await db.collection('authors').updateOne({'_id': new ObjectId(id)}, {'$set': {...objectVals}})
+}
+
+export const deleteAuthor = async (id:string) => {
+    const db = await database()
+
+    await db.collection('authors').deleteOne({'_id': new ObjectId(id)})
+}
 
 export const getAllAuthors = async () => {
     const db = await database()
