e3767bdff7e15d094d36027bc3e98c75016b6c62
Manuel <manueljoseph113@gmail.com>
2020-11-15 14:52:44 -0500

    added add, modify, delete functions

--- src/utils/genres.ts
+++ src/utils/genres.ts
@@ -1,5 +1,26 @@
 import database from '../database/database'
-import {DBGenre} from '../database/dbInterfaces'
+import {DBGenre, Genre} from '../database/dbInterfaces'
+import {ObjectId} from 'mongodb'
+
+export const addGenre = async (values:Genre) => {
+    const db = await database()
+
+    const dbOperation = await db.collection('genres').insertOne(values)
+
+    return <DBGenre>dbOperation.ops[0]
+}
+
+export const modifyGenre = async (id: string, values:Genre) => {
+    const db = await database()
+
+    await db.collection('genres').updateOne({'_id': new ObjectId(id)}, {'$set': {...values}})
+}
+
+export const deleteGenre = async (id:string) => {
+    const db = await database()
+
+    await db.collection('genres').deleteOne({'_id': new ObjectId(id)})
+}
 
 export const getAllGenres = async () => {
     const db = await database()
