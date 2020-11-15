906dc209c4f58b356a9363052c8dffa4c4320088
Manuel <manueljoseph113@gmail.com>
2020-11-15 13:16:35 -0500

    added add, modify, delete functions

--- src/utils/bites.ts
+++ src/utils/bites.ts
@@ -1,5 +1,28 @@
 import database from '../database/database'
-import {DBSpanishBite} from '../database/dbInterfaces'
+import {DBSpanishBite, ClientSpanishBite} from '../database/dbInterfaces'
+import {ObjectId} from 'mongodb'
+
+interface Values extends Omit<ClientSpanishBite, '_id'> {}
+
+export const createBite = async (values:Values) => {
+    const db = await database()
+
+    const dbOperation = await db.collection('bites').insertOne(values)
+
+    return <DBSpanishBite>dbOperation.ops[0]
+}
+
+export const modifyBite = async (id:string, values:Values) => {
+    const db = await database()
+
+    await db.collection('bites').updateOne({'_id': new ObjectId(id)}, {'$set': {...values}})
+}
+
+export const deleteBite = async (id:string) => {
+    const db = await database()
+
+    await db.collection('bites').deleteOne({'_id': new ObjectId(id)})
+}
 
 export const getAllBites = async () => {
     const db = await database()
