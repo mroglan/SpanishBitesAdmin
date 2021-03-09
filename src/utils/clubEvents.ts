9d299b0f4dcea52bcad1688a6d2d17cb7e431131
Manuel <manueljoseph113@gmail.com>
2021-03-09 15:10:32 -0500

    added getEvent function

--- src/utils/clubEvents.ts
+++ src/utils/clubEvents.ts
@@ -16,6 +16,34 @@ export const getAllEvents = async () => {
     return <DBClubEvent[]>events.data.map(d => ({...d.data, _id: d.ref.id}))
 }
 
+export const getEvent = async ({month, year}:CreateEventValues) => {
+
+    const event:any = await client.query(
+        q.Let(
+            {
+                yearRefs: q.Select(['data'], q.Paginate(q.Match(q.Index('clubEvents_by_year'), year))),
+                monthRefs: q.Select(['data'], q.Paginate(q.Match(q.Index('clubEvents_by_month'), month)))
+            },
+            q.If(
+                q.Equals(q.Count(q.Var('yearRefs')), 0),
+                null,
+                q.If(
+                    q.Equals(q.Count(q.Var('monthRefs')), 0),
+                    null,
+                    q.Map(
+                        q.Intersection(
+                            q.Var('yearRefs'), q.Var('monthRefs')
+                        ),
+                        (ref) => q.Get(ref)
+                    )
+                )
+            )
+        )
+    )
+
+    return event ? {...event[0].data, _id: event[0].ref.id} : event
+}
+
 export const verifyUniqueTime = async ({year, month}:CreateEventValues) => {
 
     const unique = await client.query(
@@ -47,7 +75,7 @@ export const verifyUniqueTime = async ({year, month}:CreateEventValues) => {
 
 export const createEvent = async (values:CreateEventValues) => {
 
-    const data = {...values, bookName: '', bookAuthor: '', bookDesc: '', bookImage: ''}
+    const data = {...values, bookName: '', bookAuthor: '', bookDesc: '', bookImage: '', posts: [], meetings: []}
 
     const newEvent:any = await client.query(
         q.Create(q.Collection('clubEvents'), {data})
