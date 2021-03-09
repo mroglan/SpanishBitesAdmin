2c1d5262daa76ebef67753cf477b648c710aa1e0
Manuel <manueljoseph113@gmail.com>
2021-03-09 08:04:50 -0500

    added create operation

---
+++ src/pages/api/bookclub/events.ts
@@ -0,0 +1,27 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import {getAllEvents, createEvent, verifyUniqueTime} from '../../../utils/clubEvents'
+import {verifyAdmin} from '../../../utils/auth'
+
+export default verifyAdmin(async function BookClubEvents(req:NextApiRequest, res:NextApiResponse) {
+
+    try {
+
+        if(req.method === 'GET') {
+            const events = await getAllEvents()
+            return res.status(200).json(events)
+        }
+
+        const {operation, values, id} = req.body
+
+        if(operation === 'create') {
+            const isUnique = await verifyUniqueTime(values)
+            if(!isUnique) return res.status(400).json({msg: 'Not unique'})
+            const newEvent = await createEvent(values)
+            return res.status(200).json({month: newEvent.month, year: newEvent.year})
+        }
+
+        return res.status(400).json({msg: 'Invalid request'})
+    } catch(e) {
+        return res.status(500).json({msg: 'Internal server error'})
+    }
+})
