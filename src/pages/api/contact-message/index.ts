fb28dbb3103b7a62f75c94995d8acae2587ceafd
Manuel <manueljoseph113@gmail.com>
2021-05-13 09:08:29 -0400

    added update

--- src/pages/api/contact-message/index.ts
+++ src/pages/api/contact-message/index.ts
@@ -1,5 +1,5 @@
 import {NextApiRequest, NextApiResponse} from 'next'
-import {getAllContactMessages} from '../../../utils/contactMessages'
+import {getAllContactMessages, updateMessage} from '../../../utils/contactMessages'
 import {verifyAdmin} from '../../../utils/auth'
 
 export default verifyAdmin(async function ContactMessage(req:NextApiRequest, res:NextApiResponse) {
@@ -9,6 +9,13 @@ export default verifyAdmin(async function ContactMessage(req:NextApiRequest, res
         if(req.method === 'GET') {
             const msgs = await getAllContactMessages()
             return res.status(200).json(msgs)
+        }
+
+        const {operation, id, values} = req.body
+
+        if(operation === 'update') {
+            await updateMessage(id, values)
+            return res.status(200).json({msg: 'updated...'})
         }
 
         return res.status(422).json({msg: 'Invalid request'})
